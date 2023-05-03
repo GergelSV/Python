487a403784df950f3ffaf0759edec868995e1cbc
GergelSV <ttk-pp-ua@ukr.net>
2023-05-03 11:14:17 +0200

    Всі домашки по циклам виконані

--- python/HomeWork/pr03/par2/HomeWork3_4.py
+++ python/HomeWork/pr03/par2/HomeWork3_4.py
@@ -6,37 +6,13 @@ rez=0
 while  buf:
     count+=1
     buf//=10
-#print (count)
 buf=n
+i=0
 while count:
     number=buf%10
-    print (number)
     buf//=10
     if not (number ==3 or number ==6):
-        rez=rez+(number*10**count)
-        print('rez',rez)
+        rez=rez+(number*10**i)
+        i+=1
     count-=1
-print(rez)
-
-#number_of_signs=count
-#for i in range(1,number_of_signs+1):
-    
-# a=n%10
-# n //= 10
-# b=n%10
-# n //= 10
-# c=n%10
-# n //= 10
-# d=n%10
-# n //= 10
-# e=n%10
-# n //= 10
-# f=n%10
-# n //= 10
-# print (f,e,d,c,b,a)
-# if (f==0 or n):
-#     print ('No 6 digit')
-# else:
-#     a,f = f,a
-#     b,e = e,b
-#     print (f,e,d,c,b,a)
+print('rezult =',rez )
