ebbc6ca9e7ddd6111bd0d45c5cdbbd24fb6f47fb
GergelSV <ttk-pp-ua@ukr.net>
2023-05-18 22:41:11 +0200

    д/з функції ч 1. ОК

--- python/HomeWork/pr05/par02/HomeWork5.py
+++ python/HomeWork/pr05/par02/HomeWork5.py
@@ -1,14 +1,13 @@
-def multiplicationOfNumbers (a):
-    if type(a)==list:
-        total=1
-        for i in range(len(a)):
-            total*=a[i]
-        return total
+def elements_of_two_lists (ls1,ls2,ls3):
+    
+    ls3=ls1+ls2
+    return ls3
 
 
 import random
 mas1 = []
 mas2 = []
+mas12= []
 
 numb1=int(input('Input the number of numbers in the 1 list = '))
 for i in range(numb1):
@@ -17,7 +16,8 @@ for i in range(numb1):
 numb2=int(input('Input the number of numbers in the 2 list = '))
 for i in range(numb2):
     mas2.append(random.randint(-250,50))
+
+print('1 List:  ',mas1)
 print('2 List:  ',mas2)
 
-
-print('Multiplication of numbers lists = ',multiplicationOfNumbers(mas))
+print('The third list containing elements common to two lists',elements_of_two_lists(mas1,mas2,mas12))
